87eb7feae255f621a3c468ad4429b9ec64ed44d4
Amr Elzafarani <amrelza3frany@gmail.com>
2022-05-10 07:12:55 +0200

    Adding setAvatars route

--- public/chat-app/src/utilis/APIRoutes.js
+++ public/chat-app/src/utilis/APIRoutes.js
@@ -1,3 +1,4 @@
 const host = "http://localhost:5000";
 export const registerRoute = `${host}/users/register`;
 export const loginRoute = `${host}/users/login`;
+export const setAvatarRoute = `${host}/users/setAvatar`;

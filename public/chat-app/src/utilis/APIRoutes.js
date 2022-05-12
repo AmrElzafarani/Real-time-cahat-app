508ca37bbd05023e2659941d37f1b3a9308d3e5c
Amr Elzafarani <amrelza3frany@gmail.com>
2022-05-12 02:02:29 +0200

    Adding get users Route

--- public/chat-app/src/utilis/APIRoutes.js
+++ public/chat-app/src/utilis/APIRoutes.js
@@ -2,3 +2,4 @@ const host = "http://localhost:5000";
 export const registerRoute = `${host}/users/register`;
 export const loginRoute = `${host}/users/login`;
 export const setAvatarRoute = `${host}/users/setAvatar`;
+export const allUsersRoute = `${host}/users/allUsers`;

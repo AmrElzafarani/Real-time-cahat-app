f7d5401473d4ed8d4dfb3ae59935e18a3942c397
Amr Elzafarani <amrelza3frany@gmail.com>
2022-05-14 08:57:32 +0200

    Adding messages Routes

--- public/chat-app/src/utilis/APIRoutes.js
+++ public/chat-app/src/utilis/APIRoutes.js
@@ -1,5 +1,7 @@
-const host = "http://localhost:5000";
+export const host = "http://localhost:5000";
 export const registerRoute = `${host}/users/register`;
 export const loginRoute = `${host}/users/login`;
 export const setAvatarRoute = `${host}/users/setAvatar`;
 export const allUsersRoute = `${host}/users/allUsers`;
+export const sendMessageRoute = `${host}/messages/addMsg`;
+export const getAllMessagesRoute = `${host}/messages/getMsg`;

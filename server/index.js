ee80152ff7781c5c29e4d453e31a8c605cec899b
Amr Elzafarani <amrelza3frany@gmail.com>
2022-05-14 09:00:25 +0200

    Import socket.io

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import  {routes} from "./routes/userRoutes.js";
+import  {routes} from "./routes/routes.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+
 const app = express();
 dotenv.config();
 
@@ -19,7 +23,37 @@ mongoose.connect(process.env.MONGO_URL, {
 }).catch((err) => {
     console.log(err.message)
 });
+const httpServer = createServer(app);
 
-app.listen(parseInt(process.env.PORT), ()=> {
-    console.log("Server started")
+app.set('port', 5000);
+
+httpServer.listen(app.get('port'), function () {
+    const port = httpServer.address();
+    console.log('Running on : ', port);
+});
+//  const server = app.listen(parseInt(process.env.PORT), ()=> {
+//     console.log("Server started")
+// })
+
+const io =  new Server(httpServer, {
+    cors: {
+        origin: "https://localhost:3000",
+        credentials: true,
+    },
+});
+global.onlineUsers = new Map();
+
+io.on("connection", (Server) => {
+    global.chatSocket = Server;
+    Server.on("add-user", (userId) => {
+        onlineUsers.set(userId, Server.id);
+    });
+    Server.on("send-msg", (data) => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if(sendUserSocket) {
+            Server.to(sendUserSocket).emit("msg-receive", data.msg)
+        }
+    })
 })
+
+

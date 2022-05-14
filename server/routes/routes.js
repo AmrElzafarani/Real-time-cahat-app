7da728496eeb7bfa4aa825e592ed919820266df2
Amr Elzafarani <amrelza3frany@gmail.com>
2022-05-14 09:03:20 +0200

    Adding Messages Routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,9 @@
 import {UserController} from "../controllers/usersController.js"
+import {MessageController} from "../controllers/messagesController.js";
 
 const _routes = [
-    ['/users', UserController]
+    ['/users', UserController],
+    ['/messages', MessageController]
 ];
 
 export const routes = (app) => {
